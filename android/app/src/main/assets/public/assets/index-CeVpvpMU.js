76720f59290851244387a9d304b4d06fcb0e6334
Luis Acevedo <laar@pm.me>
2026-05-29 22:07:01 -0400

    feat: New app icon (Robin bird) + Adaptive Icons v2.3.1
    NEW APP ICON:
    - Beautiful geometric bird icon (low-poly style)
    - Orange/white bird on mint background
    - Sound waves indicating voice/audio functionality
    
    ANDROID ADAPTIVE ICONS (Option B):
    - Generated in all densities: mdpi, hdpi, xhdpi, xxhdpi, xxxhdpi
    - Both square and round versions
    - Foreground: bird icon
    - Background: mint color (#D4E8E0)
    - Adaptive XMLs for Android 8.0+ (mipmap-anydpi-v26)
    - Legacy icons for older Android versions
    
    FILES GENERATED:
    - 5 densities x 2 shapes = 10 standard icons
    - 5 foreground drawables for adaptive icons
    - 5 background colors
    - 2 adaptive-icon XMLs
    
    Version: 2.3.1 (versionCode 17)

---
+++ android/app/src/main/assets/public/assets/index-CeVpvpMU.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DBwe8oBK.js","assets/index-C7cCWaN8.js","assets/index-B-Ue1BYn.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-C7cCWaN8.js";const n=r("Preferences",{web:()=>t(()=>import("./web-DBwe8oBK.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
